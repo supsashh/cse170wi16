9bfa842529ab78f36f41a015c867afb5cb890d6b
Alex G <agoldsht@ucsd.edu>
2016-01-27 18:19:07 -0800

    First commit... :(

---
+++ server.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+
+var index = require('./routes/index');
+// Example route
+// var user = require('./routes/user');
+
+var server = express();
+
+// all environments
+server.set('port', process.env.PORT || 3000);
+server.set('views', path.join(__dirname, 'views'));
+server.engine('handlebars', handlebars());
+server.set('view engine', 'handlebars');
+server.use(express.favicon());
+server.use(express.logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded());
+server.use(express.methodOverride());
+server.use(express.cookieParser('GoTo Secret Key'));
+server.use(express.session());
+server.use(server.router);
+server.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == server.get('env')) {
+  server.use(express.errorHandler());
+}
+
+// Add routes here
+server.get('/', index.view);
+// Example route
+// server.get('/users', user.list);
+
+http.createServer(server).listen(server.get('port'), function(){
+  console.log('Express server listening on port ' + server.get('port'));
+});
