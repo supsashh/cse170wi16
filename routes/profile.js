41951e8d0222fab041090f1b1ef8081dbeed07db
Samantha Hahn <smhahn@ucsd.edu>
2016-02-03 23:39:15 -0800

    Post history on profile page

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,10 @@ exports.view = function(req, res){
   res.render('profile', {
   	'image': 'profile-icon.png',
   	'name': 'John Doe',
-
+  	'restaurant1': '@ShikuSushi',
+  	'restaurant1-description': 'The jewel roll was amazing!',
+  	'food-image': 'jewel-roll.jpg',
+  	'restaurant2': '@BreakfastRepublic',
+  	'restaurant2-description': 'Get the flight of pancakes!'
   });
 };
