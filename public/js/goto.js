5af5c026cbcaf873acc9b5feec46f3e5cf98e549
Samantha Hahn <smhahn@ucsd.edu>
2016-02-07 22:42:09 -0800

    Edit tastes on settings page

--- public/js/goto.js
+++ public/js/goto.js
@@ -12,4 +12,23 @@ $(document).ready(function() {
     	window.location.href = $(this).data("url");
     }); 
 
+    $(document).on("tap vclick click", ".taste-delete", function(event) {
+      var $this;
+      event.preventDefault();
+      event.stopPropagation();
+      $this = $(this);
+      $this.closest('.taste').remove();
+    });
+
+    $(document).on("tap vclick click", ".add-taste-button", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      var text = $('#add-taste-input').val();
+      var taste = "<div class='taste'><span class='label taste-label'>";
+      taste = taste + text + "</span><a class='taste-delete' href='#'>×</a></div>";
+      $('.tastes').append(taste);
+      $('#add-taste-input').val("");
+    });
+
+
 });
