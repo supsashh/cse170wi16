81bac60c06cbc3c87ac8631d0a8f255915db08f2
Marvin Salazar <msalazar42@me.com>
2016-02-19 02:43:01 -0800

    added fake friends to friends list and gave them fake profiles

--- routes/friendprofile.js
+++ routes/friendprofile.js
@@ -2,5 +2,28 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	data.fb_id = req.params.id;
-	res.render('friendprofile', data);
+
+	var friendName = req.params.id;
+	var friends = data.friends;
+	var friend;
+	for(var i = 0; i< friends.length; i++)
+	{
+		if(friendName == friends[i].name)
+		{
+			friend = friends[i];
+
+			break;
+		}
+	}
+
+	if(friend)
+	{
+		res.render('friendprofile', friend);
+	}
+	else
+	{
+		res.render('friendprofile', data);
+	}
+	
+	
 };
