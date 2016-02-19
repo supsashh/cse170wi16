efd36bc4c50aada20b1e9adb0347040585a223f3
Marvin Salazar <msalazar42@me.com>
2016-02-18 22:32:40 -0800

    comments work everywhere. post json consistant

--- public/js/goto.js
+++ public/js/goto.js
@@ -75,13 +75,13 @@ $(document).ready(function() {
 
       $.post("/newpost/addpost/", { "id": postId,
         "food-image": post_img,
-        "name": "John Johnson",
-        "profile-image": "profile-icon.png",
+        "name": "",
+        "profile-image": "",
         "restaurant": post_tag,
         "restaurant-description": restaurant_desc,
         "comments": [ { 
-          "commenter": "John Johnson",
-          "commenter-image": "profile-icon.png",
+          "commenter": "",
+          "commenter-image": "",
           "text": restaurant_desc } ]
          }, function() {
           window.location.href= "/newsfeed";
@@ -95,21 +95,17 @@ $(document).ready(function() {
         var postpath = window.location.pathname;
         var strArr = postpath.split("/");
         var postId = strArr[strArr.length - 1];
-        var code = "<li class='media'><a class='pull-left' href='#''>" +
-              "<img class='media-object img-circle' src='/images/profile-icon.png' alt='profile'>" +
-              "</a><div class='media-body'><div class='well well-sm'>" + 
-              "<h4 class='media-heading reviews'>John Johnson</h4>" +
-              "<p class='media-comment'>";
-        code = code + comment_text + "</p></div></div></li>";
+        
 
 
-        $.post("/comments/newcomment/"+postId, { "commenter": "John Johnson",
-          "commenter-image": "profile-icon.png",
+        $.post("/comments/newcomment/"+postId, { "commenter": "",
+          "commenter-image": "",
           "text": comment_text
           }, function() {
-            $('.media-list').append(code);
+            
             $('#addcomment').val("");
             $('#addcomment').blur();
+            window.location.href = "/comments/" + postId;
           });
     });
 
